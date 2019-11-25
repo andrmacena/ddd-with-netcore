fd63f2b80db652f3cb56b94e44d3d01c526145ed
Andre Macena <andrmacena_@outlook.com>
2019-11-25 17:56:42 -0300

    Refatorando testes

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaymentContext.Domain.Entities;
 
@@ -10,7 +11,7 @@ namespace PaymentContext.Tests
         public void AdicionarAssinatura()
         {
             var subscription = new Subscription(null);
-            var student = new Student("André","Macena","12345678912","andre@macena.com");
+            var student = new Student(new Domain.ValueObjects.Name("Andre","Macena"), new Domain.ValueObjects.Document("12345678912",new Domain.Enums.EDocumentType()),new Domain.ValueObjects.Email("andre@teste.com"));
             student.AddSubscription(subscription);
         }
     }
