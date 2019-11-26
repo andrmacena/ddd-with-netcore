bf203b0d70224b9a0b2834258ddfc1f3fc5d2b1a
Andre Macena <andrmacena_@outlook.com>
2019-11-26 13:34:13 -0300

    Refatorando o código

--- PaymentContext.Domain/Entities/CreditCardPayment.cs
+++ PaymentContext.Domain/Entities/CreditCardPayment.cs
@@ -5,7 +5,7 @@ namespace PaymentContext.Domain.Entities
 {
     public class CrediCardPayment : Payment
     {
-        public CrediCardPayment(string cardHolderName, string cardNumber, string lastTransactionNumber, DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, Document document, string payer, string address, Email email) : base(paidDate, expireDate, total, totalPaid, document, payer, address, email)
+        public CrediCardPayment(string cardHolderName, string cardNumber, string lastTransactionNumber, DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, Document document, string payer, Address address, Email email) : base(paidDate, expireDate, total, totalPaid, document, payer, address, email)
         {
             CardHolderName = cardHolderName;
             CardNumber = cardNumber;
--- PaymentContext.Domain/ValueObjects/Name.cs
+++ PaymentContext.Domain/ValueObjects/Name.cs
@@ -1,6 +1,8 @@
+using PaymentContext.Shared.Entities;
+
 namespace PaymentContext.Domain.ValueObjects
 {
-    public class Name
+    public class Name: ValueObject
     {
         public Name(string firstName, string lastName)
         {
